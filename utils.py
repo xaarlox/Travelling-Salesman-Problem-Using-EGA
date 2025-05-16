734df5ed432e779b4ff9ec4f28fc9c8f26fd621e
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-16 19:55:24 +0300

    feat: added get_user_input

--- utils.py
+++ utils.py
@@ -9,3 +9,26 @@ def calc_distance(cities):
         city_b = cities[(i + 1) % len(cities)]
         total_sum += math.hypot(city_b[1] - city_a[1], city_b[2] - city_a[2])
     return total_sum
+
+
+# Функція, яка приймає введені дані від користувача
+def get_user_input():
+    pop_size = int(input("Population size: "))
+    tournament_size = int(input("Tournament size: "))
+    mutation_rate = float(input("Mutation rate (0.0 - 1.0): "))
+    crossover_rate = float(input("Crossover rate (0.0 - 1.0): "))
+    quality = int(input("Quality of the initial population (1 - 10): "))
+    quality = max(1, min(10, quality))
+    max_gen_input = input("Maximum number of generations (enter -1 for 'to the last living'): ").strip()
+    max_generations = int(max_gen_input)
+    stop_if_no_improve = int(input("Stop if there is no improvement for N generations: "))
+
+    return {
+        "pop_size": pop_size,
+        "tournament_size": tournament_size,
+        "mutation_rate": mutation_rate,
+        "crossover_rate": crossover_rate,
+        "quality": quality,
+        "max_generations": max_generations,
+        "stop_if_no_improve": stop_if_no_improve
+    }
