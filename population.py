e642f7aae51da52f0217904f039dc54d17350d55
Valeriia Fedorenko <xaarlox@gmail.com>
2025-05-16 19:54:13 +0300

    feat: improve select_population

--- population.py
+++ population.py
@@ -3,11 +3,15 @@ from utils import calc_distance
 
 
 # Файл містить функцію select_population, яка генерує початкову популяцію можливих маршрутів для елітного генетичного алгоритму
-def select_population(cities, size):
+def select_population(cities, size, quality=5):
+    multiplier = 1 + (quality - 5) * 0.2
+    generated_size = int(size * multiplier)
+
     population = []
-    for i in range(size):
+    for i in range(generated_size):
         c = cities.copy()
         random.shuffle(c)
         distance = calc_distance(c)
-        population.append([distance, c])
-    return sorted(population)
+        population.append((distance, c))
+    population = sorted(population)
+    return population[:size]
